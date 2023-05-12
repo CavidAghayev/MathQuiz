2bb3a21095812aa870fbb53bdc7fcbd99346868e
CavidAghayev <cavidaga307@gmail.com>
2023-05-12 09:15:56 +0400

    get

--- src/pages/Exams/Exams.jsx
+++ src/pages/Exams/Exams.jsx
@@ -2,6 +2,7 @@ import styles from '../Exams/Exams.module.css'
 import Input from '../../sections/Input/Input'
 import { useState } from 'react'
 import { Select } from '../../sections/Select/Select'
+import Arrow from '../../assets/images/arrow-down.svg'
 export const Exams = () => {
    const [formData,setFormData] = useState({});
   return (
@@ -22,6 +23,7 @@ export const Exams = () => {
           </div>
           <div className={styles.select}>
             <div className={styles.select__type}>
+              <img src={Arrow} alt="" />
             <Select
             size={"sm"}
             >
@@ -32,9 +34,14 @@ export const Exams = () => {
             </Select>
             </div>
             <div className={styles.select__price}>
+            <img src={Arrow} alt="" />
             <Select
             size={"sm"}
             >
+              <option value="a">a</option>
+              <option value="b">b</option>
+              <option value="c">c</option>
+              <option value="d">d</option>
               </Select>
             </div>
           </div>
