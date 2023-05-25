0c611d3e65f9d9592809180f54f128158ac893b3
CavidAghayev <cavidaga307@gmail.com>
2023-05-25 17:28:16 +0400

    get

--- src/companents/VideoCard/VideoCard.jsx
+++ src/companents/VideoCard/VideoCard.jsx
@@ -2,18 +2,31 @@ import styles from "../VideoCard/VideoCard.module.css";
 import Button from "../../companents/Button/Button";
 import Video from "../../assets/video/video (2).mp4";
 import { useRef } from "react";
+import {ReactComponent as PlayButton } from "../../assets/images/Vector.svg" 
+import {ReactComponent as PauseButton } from "../../assets/images/pause-button-svgrepo-com (2).svg" 
 export const VideoCard = ({ name, questionCount, classNumber }) => {
   const videoRef = useRef();
-
+  const buttonRef = useRef();
+  const pauseButtonRef = useRef();
+  const playButtonRef = useRef();
   return (
     <div className={styles.videocard}>
       <div className={styles.video}>
+        <div className={styles.overlay}>
+        <PauseButton ref={pauseButtonRef} className={styles.pause}/>
         <div
           onClick={() => {
             videoRef.current.play();
+            if(playButtonRef.current.style.display = 'none'){
+              pauseButtonRef.current.style.display = "block"
+            }
           }}
           className={styles["play-btn"]}
-        ></div>
+          ref={buttonRef}
+        >
+            <PlayButton ref={playButtonRef} className={styles.play}/>
+        </div>
+        </div>
         <video ref={videoRef} style={{ width: "100%", borderRadius: "10px" }}>
           <source src={Video} type="video/mp4" />
         </video>
